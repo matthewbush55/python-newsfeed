ca7cfe23483c9c22ab8b95e766cbd441db1df66b
Matthew Bush <mbush@terrapin.care>
2022-02-03 14:13:16 -0700

    Added route security

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -1,13 +1,35 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, session
+from app.models import Post
+from app.db import get_db
+from app.utils.auth import login_required
 
 bp_dashboard = Blueprint('dashboard', __name__, url_prefix='/dashboard')
 
 @bp_dashboard.route('/')
+@login_required
 def dashboard():
-    return render_template('dashboard.html')
+    db = get_db()
+    posts = (
+        db.query(Post)
+        .filter(Post.user_id == session.get('user_id'))
+        .order_by(Post.created_at.desc())
+        .all()
+    )
+
+    return render_template('dashboard.html', posts=posts, loggedIn=session.get('loggedIn'))
 
 @bp_dashboard.route('/edit/<id>')
+@login_required
 def edit(id):
-    return render_template('edit-post.html')
+    # get single post by id
+    db = get_db()
+    post = db.query(Post).filter(Post.id == id).one()
+
+    # render edit page
+    return render_template(
+        'edit-post.html',
+        post=post,
+        loggedIn=session.get('loggedIn')
+    )
 
 
